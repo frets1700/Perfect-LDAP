86c4499d7c6022319a9f0861ddb9bc6ca669f647
frets1700 <panmol1700@gmail.com>
2021-12-24 10:19:57 -0500

    fixes for swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,14 +11,20 @@ let package = Package(
             targets: ["PerfectLDAP"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/PerfectSideRepos/Perfect-ICONV.git", from: "3.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-libSASL.git", from: "1.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-OpenLDAP.git", from: "1.0.0"),
+        .package(name: "PerfectICONV", url: "https://github.com/frets1700/Perfect-ICONV.git", .branch("master")),
+        .package(name: "SASL", url: "https://github.com/PerfectlySoft/Perfect-libSASL.git", from: "1.0.0"),
+        .package(name: "OpenLDAP", url: "https://github.com/PerfectlySoft/Perfect-OpenLDAP.git", from: "1.0.0"),
     ],
     targets: [
+        .target(name: "CTime", dependencies: []),
         .target(
             name: "PerfectLDAP",
-            dependencies: ["PerfectICONV"]),
+            dependencies: [
+                .target(name: "CTime"),
+                .product(name: "PerfectICONV", package: "PerfectICONV"),
+                .product(name: "SASL", package: "SASL"),
+                .product(name: "OpenLDAP", package: "OpenLDAP")
+            ]),
         .testTarget(
             name: "PerfectLDAPTests",
             dependencies: ["PerfectLDAP"]),
